0215a484dfdd6cf690ef8b5a51aa8a5ac8f0b06a
Haven <haven@enclave.vn>
2017-03-22 14:12:34 +0700

    Change variable name

--- routes/todo.js
+++ routes/todo.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var Authenticator = require('../Authenticator');
+var authenticator = require('../Authenticator');
 var router = express.Router();
 var todoController = require('../controllers/TodoController');
 
-router.post('/todo', Authenticator, todoController.post)
-router.get('/todos', Authenticator, todoController.list);
-router.put('/todo', Authenticator, todoController.update);
+router.post('/todo', authenticator, todoController.post)
+router.get('/todos', authenticator, todoController.list);
+router.put('/todo', authenticator, todoController.update);
 
 module.exports = router;
